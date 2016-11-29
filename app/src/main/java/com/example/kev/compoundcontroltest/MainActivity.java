261c97b3b399988dc5fcbc2706fb2b55511402c7
KevThompson2020 <4nge1Buffy>
2016-11-29 17:35:33 +0000

    starting to put in a HAPI text entry.

--- app/src/main/java/com/example/kev/compoundcontroltest/MainActivity.java
+++ app/src/main/java/com/example/kev/compoundcontroltest/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.kev.compoundcontroltest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.kev.compoundcontroltest.compoundComponents.SideSpinner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializes the side spinner from code.
+        SideSpinner fruitsSpinner;
+        fruitsSpinner = (SideSpinner)this.findViewById(R.id.sidespinner_fruits);
+
+        CharSequence fruitList[] = { "Apple",
+                "Orange",
+                "Pear",
+                "Grapes" };
+        fruitsSpinner.setValues(fruitList);
+        fruitsSpinner.setSelectedIndex(1);
     }
+
 }
+
+
